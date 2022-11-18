dabf19531d20a1c56177aa0d07eac64883786b37
Alistair Keiller <alistair@keiller.net>
2022-11-18 06:28:51 -0800

    less nessesary logic

--- rl.jl
+++ rl.jl
@@ -1,15 +1,13 @@
 using ReinforcementLearning
 
 mutable struct PodRacerEnv <: AbstractEnv
-    pos::Tuple{Float64,Float64}
+    distance_to_checkpoints::Vector{Tuple{Float64,Float64}}
     vel::Tuple{Float64,Float64}
     angle::Float64
-    checkpoints::Vector{Tuple{Float64,Float64}}
-    current_checkpoint::Int
     reward::Float64
     max_angle_change::Float64 = 15
     checkpoints_size::Float64 = 150
     time_step::Float64 = 0.1
 end
 
-RLBase.action_space(env::PodRacerEnv) = env.
+RLBase.action_space(env::PodRacerEnv) = (0:100, -15:15, Bool)
