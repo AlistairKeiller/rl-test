5325205438365280ce5e248b95b581004722e14c
Alistair Keiller <alistair@keiller.net>
2022-11-18 21:03:06 -0800

    add more

--- rl.jl
+++ rl.jl
@@ -3,11 +3,14 @@ using IntervalSets
 using ReinforcementLearning
 
 mutable struct PodRacerEnv <: AbstractEnv
-    state::Tuple{Float64,Tuple{FLoat64,Float64},Tuple{Tuple{Float64,Float64},Tuple{Float64,Float64}}} # (angle, velocity, pos of next 2 checkpoints)
+    state::Tuple{Float64,FLoat64,Float64,Float64,Float64,Float64,Float64} # (angle, vel1, vel2, pos1 of current checkpoint, pos2 of current checkpoint, pos1 of next checkpoint, pos2 of next checkpoint)
     reward::Float64
+    is_terminated::Bool
 end
 
 RLBase.action_space(env::PodRacerEnv) = ClosedInterval[0..100, -15..15]
-RLBase.state_space(env::PodRacerEnv) = Space(ClosedInterval[0..100, -15..15])
+RLBase.state_space(env::PodRacerEnv) = Space(ClosedInterval[0..360, 0..200, 0..200, 0..3000, 0..3000, 0..3000, 0..3000])
 RLBase.state(env::PodRacerEnv) = env.state
 RLBase.reward(env::PodRacerEnv) = env.reward
+RLBase.is_terminated(env::PodRacerEnv) = env.is_terminated
+RLBase.reset!(env::PodRacerEnv) = env.reward = 0
